18669010551e4e7833cbd50a5167ccd90f3e3df5
yuan-wei-hua <2964966950@qq.com>
2025-08-11 23:07:59 +0800

    Add files via upload

---
+++ code2025.sql
@@ -0,0 +1,28 @@
+﻿# Host: localhost  (Version: 5.7.26)
+# Date: 2025-08-11 23:03:57
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "admin"
+#
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '手机',
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='管理员信息\r\n';
+
+#
+# Data for table "admin"
+#
+
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'yuanweihua','han102513','yuan','18559151778','2964966950@qq.com');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
