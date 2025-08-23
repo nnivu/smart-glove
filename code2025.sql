e484a2e2ed0abff76aa64d9efadc6a49518bb48d
yuan-wei-hua <2964966950@qq.com>
2025-08-23 18:36:40 +0800

    Add files via upload

--- code2025.sql
+++ code2025.sql
@@ -1,28 +1,63 @@
-﻿# Host: localhost  (Version: 5.7.26)
-# Date: 2025-08-20 07:46:19
-# Generator: MySQL-Front 5.3  (Build 4.234)
+/*
+ Navicat Premium Dump SQL
 
-/*!40101 SET NAMES utf8 */;
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50726 (5.7.26)
+ Source Host           : localhost:3306
+ Source Schema         : code2025
 
-#
-# Structure for table "admin"
-#
+ Target Server Type    : MySQL
+ Target Server Version : 50726 (5.7.26)
+ File Encoding         : 65001
 
+ Date: 23/08/2025 18:30:15
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
 DROP TABLE IF EXISTS `admin`;
-CREATE TABLE `admin` (
+CREATE TABLE `admin`  (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '账号',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '手机',
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '邮箱',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='管理员信息\r\n';
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '手机',
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '角色',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '管理员信息\r\n' ROW_FORMAT = Dynamic;
 
-#
-# Data for table "admin"
-#
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+INSERT INTO `admin` VALUES (1, 'yuanweihua', '123456', 'yuan', '18559151778', '2964966950@qq.com', 'ADMIN');
+INSERT INTO `admin` VALUES (2, 'admin', '123456', 'admin', NULL, NULL, 'ADMIN');
+INSERT INTO `admin` VALUES (3, 'Sun Rui', 'z6Y4CEPDMB', 'Sun Rui', '74-573-4982', 'sun10@mail.com', 'ADMIN');
+INSERT INTO `admin` VALUES (4, 'Cui Xiaoming', 'WxUMVfM19L', 'Cui Xiaoming', '(1865) 44 7654', 'xiaoming1@icloud.com', 'ADMIN');
+INSERT INTO `admin` VALUES (5, 'Kwan Wing Fat', 'awlb2h8T6E', 'Kwan Wing Fat', '52-572-4512', 'kwanwf@outlook.com', 'ADMIN');
 
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'yuanweihua','123456','yuan','18559151778','2964966950@qq.com'),(2,'admin','123456','admin',NULL,NULL);
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '密码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '手机',
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '角色',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
